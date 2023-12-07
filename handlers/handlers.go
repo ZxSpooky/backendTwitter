2e11ea429b54c847bee71d5a88c8a518a8eeeb96
ZxSpooky <ricardo13work@gmail.com>
2023-12-07 03:17:45 -0300

    Ultimo Commit

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/ZxSpooky/backendTwitter/jwt"
 	"github.com/ZxSpooky/backendTwitter/models"
+	"github.com/ZxSpooky/backendTwitter/routers"
 	"github.com/aws/aws-lambda-go/events"
+	
 )
 
 func Manejadores(ctx context.Context, request events.APIGatewayProxyRequest) models.RespApi {
@@ -14,9 +17,18 @@ func Manejadores(ctx context.Context, request events.APIGatewayProxyRequest) mod
 	var r models.RespApi
 	r.Status = 400
 
+	isOK, statusCode, msg, _ := validoAuthorization(ctx, request)
+	if !isOK {
+		r.Status = statusCode
+		r.Message = msg
+		return r
+	}
+
 	switch ctx.Value(models.Key("method")).(string) {
 	case "POST":
 		switch ctx.Value(models.Key("path")).(string) {
+		case "registro":
+			return routers.Registro(ctx)
 
 		}
 		//
@@ -38,4 +50,27 @@ func Manejadores(ctx context.Context, request events.APIGatewayProxyRequest) mod
 	}
 	r.Message = "Methos Invalid"
 	return r
+}
+
+func validoAuthorization(ctx context.Context, request events.APIGatewayProxyRequest) (bool, int, string, models.Claim) {
+	path := ctx.Value(models.Key("path")).(string)
+	if path == "registro" || path == "login" || path == "obtenerAvatar" || path == "obtenerBanner" {
+		return true, 200, "", models.Claim{}
+	}
+	token := request.Headers["Authorization"]
+	if len(token) == 0 {
+		return false, 401, "Token requerido", models.Claim{}
+	}
+	claim, todoOK, msg, err := jwt.ProcesoToken(token, ctx.Value(models.Key("jwtSign")).(string))
+	if !todoOK {
+		if err != nil {
+			fmt.Println("Error en el token " + err.Error())
+			return false, 401, err.Error(), models.Claim{}
+		} else {
+			fmt.Println("Error en el token " + msg)
+			return false, 401, msg, models.Claim{}
+		}
+	}
+	fmt.Println("Token OK")
+	return true, 200, msg, *claim
 }
---
+++ jwt/procesoToken.go
@@ -0,0 +1,35 @@
+package jwt
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/ZxSpooky/backendTwitter/models"
+	jwt "github.com/golang-jwt/jwt/v5"
+)
+
+var Email string
+var IDUsuario string
+
+func ProcesoToken(tk string, JWTSign string) (*models.Claim, bool, string, error) {
+
+	miClave := []byte(JWTSign)
+	var claims models.Claim
+
+	splitToken := strings.Split(tk, "Bearer")
+	if len(splitToken) != 2 {
+		return &claims, false, string(""), errors.New("formato de token invalido")
+	}
+	tk = strings.TrimSpace(splitToken[1])
+
+	tkn, err := jwt.ParseWithClaims(tk, &claims, func(t *jwt.Token) (interface{}, error) {
+		return miClave, nil
+	})
+	if err == nil {
+		//Rutina que chequea contra la BD
+	}
+	if !tkn.Valid {
+		return &claims, false, string(""), errors.New("Token Invalido")
+	}
+	return &claims, false, string(""), err
+}
