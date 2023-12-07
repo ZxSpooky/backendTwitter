74412ccd3b5d3981089bb5c2493156e4772aa3f2
ZxSpooky <ricardo13work@gmail.com>
2023-12-07 01:34:23 -0300

    Ultimo Commit

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"os"
+	"strings"
 
 	"github.com/ZxSpooky/backendTwitter/awsgo"
+	"github.com/ZxSpooky/backendTwitter/bd"
+	"github.com/ZxSpooky/backendTwitter/handlers"
 	"github.com/ZxSpooky/backendTwitter/models"
 	"github.com/ZxSpooky/backendTwitter/secretmanager"
 	"github.com/aws/aws-lambda-go/events"
@@ -30,16 +33,55 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (
 		return res, nil
 	}
 
-		SecretModel, err:=secretmanager.GetSecret(os.Getenv("SecretName"))
-		if err != nil {
-			res = &events.APIGatewayProxyResponse{
-				StatusCode: 400,
-				Body:       "Error en la lectura de Secret"+err.Error(),
-				Headers: map[string]string{
-					"Content-Type": "application/json",
-				},
+	SecretModel, err := secretmanager.GetSecret(os.Getenv("SecretName"))
+	if err != nil {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Error en la lectura de Secret" + err.Error(),
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
 		}
-}
+	}
+	path := strings.Replace(request.PathParameters["twitterGO"], os.Getenv("UrlPrefix"), "", -1)
+
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("path"), path)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("method"), request.HTTPMethod)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("user"), SecretModel.Username)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("password"), SecretModel.Password)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("host"), SecretModel.Host)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("database"), SecretModel.Database)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("jwtSign"), SecretModel.JWTSign)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("body"), request.Body)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("bucketName"), os.Getenv("BucketName"))
+
+	//Chequeo Conexion a la BD
+
+	err = bd.ConectarBD(awsgo.Ctx)
+	if err != nil {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       "Error Conectando la BD" + err.Error(),
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+		return res, nil
+
+	}
+	respApi := handlers.Manejadores(awsgo.Ctx, request)
+	if respApi.CustomResp == nil {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: respApi.Status,
+			Body:       respApi.Message,
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+		return res, nil
+	} else {
+		return respApi.CustomResp, nil
+	}
 }
 
 func ValidoParametros() bool {
---
+++ models/types.go
@@ -0,0 +1,7 @@
+package models
+
+import (
+	
+)
+
+type Key string
